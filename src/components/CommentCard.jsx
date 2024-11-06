8c8ee59c530eaa44ac7e7ef157564f588a14201b
zajk9999 <lukasz.zajkowski@gmail.com>
2024-11-06 16:49:27 +0000

    add like button to the article, implemented with optimistic rendering

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,12 +1,10 @@
-import { Link } from "react-router-dom";
-
 export default function CommentCard(props) {
   const { comment } = props;
   return (
     <div className="comment-card">
       <h3>{comment.body}</h3>
       <p>by {comment.author}</p>
-      <p className="likes">Likes: {comment.votes}</p>
+      <p className="likes">{comment.votes} Likes</p>
     </div>
   );
 }
