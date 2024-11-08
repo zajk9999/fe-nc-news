473b4045b0e2c306de8de368d95e3e21ed8f31e1
zajk9999 <lukasz.zajkowski@gmail.com>
2024-11-08 16:05:07 +0000

    add error handling to deleting comments

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -4,17 +4,22 @@ import { deleteCommentById } from "../api";
 export default function DeleteButton(props) {
   const [isDeleting, setIsDeleting] = useState(false);
   const { comment_id } = props;
+  const [error, setError] = useState(null);
   const handleDelete = (comment_id) => {
     setIsDeleting(true);
-    deleteCommentById(comment_id);
+    deleteCommentById(comment_id).catch((err) => {
+      setIsDeleting(false);
+      setError("Something went wrong, please try again");
+    });
   };
+
   return (
     <>
       {isDeleting ? (
         <p>Deleting</p>
       ) : (
         <button id="delete-button" onClick={() => handleDelete(comment_id)}>
-          <>Delete comment</>
+          {error ? <p>{error}</p> : <>Delete comment</>}
         </button>
       )}
     </>
