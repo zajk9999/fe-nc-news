1b2b135b6e4dbbedf88e7eb1cc463a28b955fd31
zajk9999 <lukasz.zajkowski@gmail.com>
2024-11-12 22:48:26 +0000

    fixed infinte loop and improved add and delete comments functionality

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
 import { deleteCommentById } from "../api";
 
-export default function DeleteButton(props) {
+export default function DeleteButton({ comment_id, onUpdate }) {
   const [isDeleting, setIsDeleting] = useState(false);
-  const { comment_id } = props;
   const [error, setError] = useState(null);
   const handleDelete = (comment_id) => {
     setIsDeleting(true);
-    deleteCommentById(comment_id).catch((err) => {
-      setIsDeleting(false);
-      setError("Something went wrong, please try again");
-    });
+    deleteCommentById(comment_id)
+      .then(() => {
+        onUpdate();
+      })
+      .catch((err) => {
+        setIsDeleting(false);
+        setError("Something went wrong, please try again");
+      });
   };
 
   return (
