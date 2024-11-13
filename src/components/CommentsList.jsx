349f412b1cf140f575c940c6ee72c4e0e84a57c1
zajk9999 <lukasz.zajkowski@gmail.com>
2024-11-13 16:54:20 +0000

    app deployment

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -13,7 +13,6 @@ export default function CommentsList() {
 
   const handleUpdate = () => {
     setRerenderCommentsList((current) => !current);
-    console.log(rerenderCommentsList);
   };
 
   useEffect(() => {
